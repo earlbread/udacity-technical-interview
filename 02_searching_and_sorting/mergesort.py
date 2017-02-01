077fcb7dfd82382a23e4bdcf80cd87f4b5958d7d
Seunghun Lee <waydi1@gmail.com>
2017-01-31 19:30:51 -0800

    Remove solutions from problems

--- 02_searching_and_sorting/mergesort.py
+++ 02_searching_and_sorting/mergesort.py
@@ -1,39 +1,7 @@
 #!/usr/bin/env python
 
-def merge(a, b):
-    i = 0
-    j = 0
-    result = []
-
-    while i < len(a) and j < len(b):
-        if a[i] <= b[j]:
-            result.append(a[i])
-            i += 1
-        else:
-            result.append(b[j])
-            j += 1
-
-    while i < len(a):
-        result.append(a[i])
-        i += 1
-
-    while j < len(b):
-        result.append(b[j])
-        j += 1
-
-    return result
-
-
 def mergesort(array):
-    if len(array) == 1:
-        return array
-
-    mid = len(array) / 2
-
-    a = mergesort(array[:mid])
-    b = mergesort(array[mid:])
-
-    return merge(a, b)
+    pass
 
 if __name__ == '__main__':
     import random
